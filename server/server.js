aec4fe6aedd4fdc277949a9a499945565c492f3e
katiemikul <katie.heding@gmail.com>
2018-04-18 14:15:09 -0500

    added public folder with index.html, css, and client.js

--- server/server.js
+++ server/server.js
@@ -4,16 +4,22 @@ const app = express();
 const PORT = 5000;
 
 app.use(express.static('server/server'));
+app.use(express.static('server/public'));
 
 app.get('/', (req, res) => {
-    res.send('Happy Wednesday!');
+    res.send();
 });
 
 app.get('/all-quotes', (req, res) => {
     res.send(quotes_data);
 });
 
-app.listen(PORT, function () {
+app.get('/quotes', (req, res) => {
+    const randomNumber = Math.floor(Math.random()*quotes_data.length);
+    res.send(quotes_data[randomNumber]);
+});
+
+app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`)
 });
 
