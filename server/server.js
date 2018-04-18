dcc871aa10a97e92742eb5ad4c05f32046653595
katiemikul <katie.heding@gmail.com>
2018-04-18 10:17:09 -0500

    add all-quotes module

--- server/server.js
+++ server/server.js
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
     res.send('Happy Wednesday!');
 });
 
+app.get('/all-quotes', (req, res) => {
+    res.send(quotes_data);
+});
+
 app.listen(PORT, function () {
     console.log(`listening on port: ${PORT}`)
 });
+
+const quotes_data = require('./quotes_data');
